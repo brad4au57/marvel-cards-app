f32049edc2b6919f0aa6507d265eddf7a2cca629
brad4au57 <brad4au57@gmail.com>
2021-06-16 16:13:22 -0500

    Fixed missing dependency error

--- src/components/CharPicker.js
+++ src/components/CharPicker.js
@@ -19,32 +19,32 @@ export default function CharPicker(props) {
   const [activeTitle, setActiveTitle] = useState("");
   const [windowWidth, setWindowWidth] = useState(getWindowWidth());
 
-  const fetchData = () => {
-    console.log(windowWidth);
-    const series_id = 16410; //Guardians of the Galaxy (2013-2015)
-
-    fetch(seriesURL(series_id))
-      .then((res) => res.json())
-      .then(
-        (response) => {
-          const loadedSeries = {
-            id: response.data.results[0].id,
-            characters: response.data.results[0].characters.items,
-          };
-          setLoadedSeries(loadedSeries);
-          setActiveTitle(loadedSeries.characters[0].name);
-          setIsLoaded(true);
-        },
-        (error) => {
-          setError(error);
-          console.log(error); // Output error for debugging
-          setIsLoaded(true);
-        }
-      );
-  };
-
   useEffect(() => {
+    const fetchData = () => {
+      const series_id = 16410; //Guardians of the Galaxy (2013-2015)
+
+      fetch(seriesURL(series_id))
+        .then((res) => res.json())
+        .then(
+          (response) => {
+            const loadedSeries = {
+              id: response.data.results[0].id,
+              characters: response.data.results[0].characters.items,
+            };
+            setLoadedSeries(loadedSeries);
+            setActiveTitle(loadedSeries.characters[0].name);
+            setIsLoaded(true);
+          },
+          (error) => {
+            setError(error);
+            console.log(error); // Output error for debugging
+            setIsLoaded(true);
+          }
+        );
+    };
+
     fetchData();
+
     function handleResize() {
       setWindowWidth(getWindowWidth());
     }
